e9b4c918ed392a6c8da33f873d69a0b8d2c8d54b
Thomas Fussell <thomas@fussell.io>
2022-02-04 15:12:20 -0500

    try micrologger adapter

--- main.go
+++ main.go
@@ -17,16 +17,21 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	capo "sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha4"
 	capi "sigs.k8s.io/cluster-api/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	"github.com/giantswarm/dns-operator-openstack/controllers"
 	// +kubebuilder:scaffold:imports
@@ -46,11 +51,70 @@ func init() {
 }
 
 func main() {
+	err := mainE()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, microerror.Pretty(err, true))
+		os.Exit(1)
+	}
+}
+
+type loggerAdapter struct {
+	micrologger.Logger
+	verbosity int
+	names     []string
+}
+
+func (m loggerAdapter) Enabled() bool {
+	return m.verbosity > 0
+}
+
+func (m loggerAdapter) Info(msg string, keysAndValues ...interface{}) {
+	if m.verbosity < 2 {
+		return
+	}
+	m.withName().With(keysAndValues...).With("level", "info").Log("message", msg)
+}
+
+func (m loggerAdapter) Error(err error, msg string, keysAndValues ...interface{}) {
+	if m.verbosity < 1 {
+		return
+	}
+	m.withName().With(keysAndValues...).Errorf(context.Background(), err, msg)
+}
+
+func (m loggerAdapter) withName() loggerAdapter {
+	wrapperCopy := m
+	if len(m.names) == 0 {
+		wrapperCopy.Logger = m.Logger.With("name", strings.Join(m.names, "."))
+	}
+	return wrapperCopy
+}
+
+func (m loggerAdapter) V(level int) logr.Logger {
+	wrapperCopy := m
+	wrapperCopy.verbosity = level
+	return wrapperCopy
+}
+
+func (m loggerAdapter) WithValues(keysAndValues ...interface{}) logr.Logger {
+	wrapperCopy := m
+	wrapperCopy.Logger = m.Logger.With(keysAndValues...)
+	return wrapperCopy
+}
+
+func (m loggerAdapter) WithName(name string) logr.Logger {
+	wrapperCopy := m
+	wrapperCopy.names = append(m.names[:], name)
+	return wrapperCopy
+}
+
+func mainE() error {
 	var (
 		baseDomain           string
 		enableLeaderElection bool
 		managementCluster    string
 		metricsAddr          string
+		verbosity            int
 	)
 
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
@@ -60,12 +124,24 @@ func main() {
 
 	flag.StringVar(&baseDomain, "base-domain", "", "Domain for which to create the DNS entries, e.g. customer.gigantic.io.")
 	flag.StringVar(&managementCluster, "management-cluster", "", "Name of the management cluster.")
+	flag.IntVar(&verbosity, "verbosity", 3, "Name of the management cluster.")
 
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New())
+	// Create a new logger which is used by all packages.
+	logger, err := micrologger.New(micrologger.Config{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	ctrl.SetLogger(loggerAdapter{Logger: logger})
+
+	config, err := ctrl.GetConfig()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	mgr, err := ctrl.NewManager(config, ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
@@ -74,7 +150,7 @@ func main() {
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+		return microerror.Mask(err)
 	}
 
 	if err = (&controllers.OpenstackClusterReconciler{
@@ -85,13 +161,15 @@ func main() {
 		ManagementCluster: managementCluster,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "OpenstackCluster")
-		os.Exit(1)
+		return microerror.Mask(err)
 	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
+		return microerror.Mask(err)
 	}
+
+	return nil
 }
